f6934b315b16d0cb678f8b3324a0e032aeb7bb70
vipul gamit <gamitvipul1011@gmail.com>
2026-04-04 09:56:46 +0530

    Update HeroBanner.jsx

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,4 +1,4 @@
-mport { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination } from 'swiper/modules';
 
 import 'swiper/css';
