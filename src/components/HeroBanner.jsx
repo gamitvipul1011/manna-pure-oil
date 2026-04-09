055725269e292ac81ab71ed64fb413eedbb0b19c
vipul gamit <gamitvipul1011@gmail.com>
2026-04-09 10:10:28 +0530

    Update HeroBanner.jsx

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -6,7 +6,6 @@ import 'swiper/css/pagination';
 
 /* DESKTOP IMAGES */
 
-
 import family1Desktop from '../assets/Ground nut oil 1920 x 800.jpg';
 import family2Desktop from '../assets/Presentil oil 1920 x 800.jpg';
 import family3Desktop from '../assets/Family Ghee 1920 x 800.jpg';
@@ -16,7 +15,6 @@ import family3Desktop from '../assets/Family Ghee 1920 x 800.jpg';
 import family1Mobile from '../assets/Ground nut oil 1920 x 800.jpg';
 import family2Mobile from '../assets/Presentil oil 1920 x 800.jpg';
 import family3Mobile from '../assets/Family Ghee 1920 x 800.jpg';
-
 
 const slides = [
   
@@ -45,7 +43,7 @@ const HeroBanner = () => {
         pagination={{ clickable: true }}
         loop
         speed={900}
-        className="h-[60vh] md:h-[90vh]"
+        className="h-screen md:h-[90vh]"
       >
         {slides.map((slide, index) => (
           <SwiperSlide key={index}>
@@ -57,11 +55,12 @@ const HeroBanner = () => {
                 alt="banner"
                 className="hidden md:block w-full h-full object-cover object-center"
               />
+
               {/* MOBILE IMAGE */}
               <img
                 src={slide.mobileImage}
-                alt="Banner"
-                className="block md:hidden w-full h-full object-cover"
+                alt="banner"
+                className="block md:hidden w-full h-full object-cover object-center"
               />
 
             </div>
