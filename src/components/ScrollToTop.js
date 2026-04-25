79eb421dc3bf5c725f48c8ac121082d668cc72e1
vipul gamit <gamitvipul1011@gmail.com>
2026-04-26 01:31:11 +0530

    Update ScrollToTop.js

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -2,15 +2,11 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const location = useLocation();
 
   useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "instant"
-    });
-  }, [pathname]);
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
 
   return null;
 };
