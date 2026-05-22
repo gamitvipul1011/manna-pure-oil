2c279152eff9181b222573a418fc16f30683968b
vipul gamit <gamitvipul1011@gmail.com>
2026-05-22 16:25:32 +0530

    Update ProductDetail.jsx

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -382,21 +382,15 @@ activeTab === tab.key
 
 <GiOilDrum className="text-purple-700 text-xl"/>
 
-<div>
-
-{(isGu && product.descriptionGu
-? product.descriptionGu
-: product.description)
-.split("\n")
-.map((line, i) => (
-
-<p key={i} className="text-gray-700 text-sm md:text-base">
-{line}
-</p>
-
-))}
-
-</div>
+<div
+  className="text-gray-700 text-sm md:text-base leading-7"
+  dangerouslySetInnerHTML={{
+    __html:
+      isGu && product.descriptionGu
+        ? product.descriptionGu
+        : product.description,
+  }}
+></div>
 
 </div>
 
