497e1e286d5c6103667ef6b352d8915ba1d552a9
vipul gamit <gamitvipul1011@gmail.com>
2026-06-05 12:31:44 +0530

    Update Blogs.jsx

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -106,7 +106,7 @@ const Blogs = () => {
   const totalPages = Math.ceil(blogs.length / blogsPerPage);
 
   return (
-   <div className="bg-gradient-purple min-h-screen">
+   <div className="bg-[#A855F7] min-h-screen">
     
       {/* Banner */}
       <div className="relative w-full h-[180px] sm:h-[260px] md:h-[380px] lg:h-[500px] overflow-hidden">
