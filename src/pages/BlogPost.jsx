3cacb4ca7d390cd6a1154e7c50b9712f31131404
vipul gamit <gamitvipul1011@gmail.com>
2026-06-05 14:06:55 +0530

    Update BlogPost.jsx

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -1488,7 +1488,7 @@ const BlogPost = () => {
   const otherBlogs = blogData.filter((b) => b.id !== blog.id);
 
   return (
-    <div className="min-h-screen bg-[#A855F7] py-12">
+    <div className="min-h-screen bg[#9B7BC9] py-12">
       <div className="max-w-4xl mx-auto px-6">
 
         {/* Back Button */}
