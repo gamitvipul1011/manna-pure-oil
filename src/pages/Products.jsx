e380d46fc2572ada3ac96f7f2aa4dcf775af846b
vipul gamit <gamitvipul1011@gmail.com>
2026-06-06 11:14:15 +0530

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -26,7 +26,7 @@ const Products = () => {
   });
 
   return (
-    <div className="bg-[#9B7BC9] min-h-screen">
+    <div className="bg-gradient-purple min-h-screen">
 
   {/* Banner */}
  <div className="relative w-full h-[180px] sm:h-[260px] md:h-[380px] lg:h-[500px] overflow-hidden">
