91030e5cb3b833250df25973b68d0a42dcef4900
vipul gamit <gamitvipul1011@gmail.com>
2026-06-06 11:14:50 +0530

    Update RefundPolicy.jsx

--- src/pages/RefundPolicy.jsx
+++ src/pages/RefundPolicy.jsx
@@ -150,7 +150,7 @@ const RefundPolicy = () => {
   ];
 
   return (
-    <div className="bg-[#9B7BC9] min-h-screen py-16">
+    <div className="bg-gradient-purple min-h-screen py-16">
       <div className="max-w-5xl mx-auto px-6">
 
         <div className="text-center mb-12">
