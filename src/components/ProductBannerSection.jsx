20c68700aa5f9570d89d8e94b398d035bbf087f6
vipul gamit <gamitvipul1011@gmail.com>
2026-06-08 18:54:32 +0530

    Update ProductBannerSection.jsx

--- src/components/ProductBannerSection.jsx
+++ src/components/ProductBannerSection.jsx
@@ -14,7 +14,7 @@ const products = [
   { image: "/images/products/slider1 (6).jpeg" },
   { image: "/images/products/slider1 (7).jpeg" },
    { image: "/images/products/slider7.jpeg" },
-   { image: "/images/products/ honey_slide1.jpeg" },
+   { image: "/images/products/honey_slide1.jpeg" },
  
     
  
